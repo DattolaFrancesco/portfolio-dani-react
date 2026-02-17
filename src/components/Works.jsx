eade4f083b36ca41a9f084ac9fd3b7d3f157c1e5
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-17 17:15:32 +0100

    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -98,7 +98,7 @@ const Works = () => {
                     );
                   })}
                 </div>
-                <div className={`carousel-inner ${i === 1 ? "" : "shadow-custom"}`} style={{ height: i === 2 ? "47vh" : "43.3vh" }}>
+                <div className={`carousel-inner ${i === 1 ? "" : "shadow-custom"}`} style={{ height: i === 2 ? "47vh" : "60vh" }}>
                   {e.map((e, i) => {
                     return (
                       <div className={` carousel-item ${i === 0 ? "active" : ""} `} key={i}>
