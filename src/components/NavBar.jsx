4b811fb3e364c706b22fd32c55f679929b338118
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-18 22:55:18 +0100

    /

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
         }}
         style={{ cursor: "pointer" }}
       >
-        <img src="/imgLanding/Cuore.webp" alt="Logo" decoding="async" draggable="false" fetchPriority="high" />
+        <img src="/imgLanding/Cuore.webp" alt="Logo" decoding="async" draggable="false" />
       </a>
 
       <div className="list-unstyled d-flex align-items-center mb-0 bg-glass rounded-pill px-2 w-max-content gap-3 py-2">
