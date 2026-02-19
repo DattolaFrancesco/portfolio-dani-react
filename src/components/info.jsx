087ede2278c0e5d591a43c4e8eb1b3c741eb7ff1
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-19 22:04:08 +0100

    .

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -48,19 +48,19 @@ const Info = () => {
         <div id="info-cont" className="d-flex align-items-center vh-custom-info">
           <section>
             <Row className="mx-2 gap-custom-info">
-              <Col xs={12} className="fs-5 lh-sm fw-normal w-max-content moveUl">
-                <h1 className="mb-4 display-4 fw-custom-info lh-1">
-                  DANIELE
-                  <br /> <span className="underline position-relative">PIACENTILE</span>
+              <Col xs={12} className="fs-5 lh-sm fw-normal w-max-content moveUl ">
+                <h1 className="mb-4 display-4 fw-custom-info h-sm-custom-info">
+                  Daniele
+                  <br /> <span className="underline position-relative">Piacentile</span>
                 </h1>
                 <a href="tel:+393519804290" className="phone-link">
-                  <p className="m-0">+39 351 980 4290</p>
+                  <p className="m-0 text-lowercase">+39 351 980 4290</p>
                 </a>
                 <a href="mailto:ELSOLITOO10@GMAIL.COM" className="text-black text-decoration-none">
-                  <p className="m-0">ELSOLITOO10@GMAIL.COM</p>
+                  <p className="m-0 text-lowercase">ELSOLITOO10@GMAIL.COM</p>
                 </a>
                 <a href="https://www.instagram.com/_.elsolito._?igsh=czZwYzB0NTNxaWtp&utm_source=qr" className="text-black text-decoration-none">
-                  <p className="m-0">INSTAGRAM ACCOUNT</p>
+                  <p className="m-0 text-lowercase">INSTAGRAM ACCOUNT</p>
                 </a>
               </Col>
               <Col xs={12} className="d-flex justify-content-end moveImg" style={{ zIndex: "999" }}>
